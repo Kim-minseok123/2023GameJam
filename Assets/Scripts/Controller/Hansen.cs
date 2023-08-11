9e9572c10d9cb98368b5881b97303de7b1141ea0
Bedford <qmal789@dgu.ac.kr>
2023-08-11 20:35:43 +0900

    feat: 폴더 이름 변경

---
+++ Assets/Scripts/Controller/Hansen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hansen : BaseController
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
