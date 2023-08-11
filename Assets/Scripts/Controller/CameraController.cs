ec9e86d33cb1a43f109900ef29efdab9542d6d95
Bedford <qmal789@dgu.ac.kr>
2023-08-12 00:12:26 +0900

    feat : 플레이어 능력 추가

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -8,17 +8,18 @@ public class CameraController : MonoBehaviour
 {
     public Transform player;
     [SerializeField] float smoothing = 0.2f;
+    public float depth = 3f;
     [SerializeField] Vector2 minCameraBoundary;
     [SerializeField] Vector2 maxCameraBoundary;
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
     private void FixedUpdate()
     {
         if (player == null) return;
-        Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+        Vector3 targetPos = new Vector3(player.position.x, 0, this.transform.position.z);
 
         targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
         targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
