5defe4de4932c33f471efd556d1c345deec740ed
zerosouth <kyns320@gmail.com>
2023-08-12 23:30:48 +0900

    [feat] 기존 레거시 씬 삭제, 프랍 브러쉬 에디터 추가

--- Assets/Scripts/GameClearEvent.cs
+++ Assets/Scripts/GameClearEvent.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class GameClearEvent : MonoBehaviour
 {
-    public string NextScene;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             //≥≤±ÿ¡° µµ¥ﬁ
             GameManager.Instance.GameClear();
-            SceneLoader.Instance.SwitchScene(NextScene);
         }
     }
 }
