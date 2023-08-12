5c3a1197e76c125955fba3fc5546751a586904fa
Bedford <qmal789@dgu.ac.kr>
2023-08-12 21:49:57 +0900

    스테이지 2 수정

--- Assets/Scripts/Controller/BaseController.cs
+++ Assets/Scripts/Controller/BaseController.cs
@@ -47,8 +47,11 @@ public class BaseController : MonoBehaviour
         if (!GameManager.Instance.isPaused)
         {
             _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, _groundLayer);
-            Collider2D cd = Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, _groundLayer);
-            if (_isGrounded && cd.gameObject.CompareTag("Fail")) { jumpRequested = false; }
+            if (!gameObject.name.Equals("Hasel"))
+            {
+                Collider2D cd = Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, _groundLayer);
+                if (_isGrounded && cd.gameObject.CompareTag("Fail")) { jumpRequested = false; }
+            }
             _moveInput = Input.GetAxis("Horizontal");
             _rb.velocity = new Vector2(_moveInput * _speed, _rb.velocity.y);
 
