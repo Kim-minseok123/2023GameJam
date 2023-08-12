4a30a0ba66f12da712cd76eae847ca107f50af50
zerosouth <kyns320@gmail.com>
2023-08-12 22:43:04 +0900

    [feat] 게임 클리어 팝업 연출

--- Assets/Scripts/UI/General/PopupOpener.cs
+++ Assets/Scripts/UI/General/PopupOpener.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 public class PopupOpener : MonoBehaviour
 {
     public string popupName;
 
+    [Button("Open")]
     public void Open()
     {
         UIController.Instance.OpenPopup(popupName);
